cf7c72307b2479657d8a77404cec1c6ad855710c
Nail <nailkalimov@gmail.com>
2020-01-17 09:52:27 +0300

    alpha version

--- src/main/DecoderService.java
+++ src/main/DecoderService.java
@@ -2,5 +2,18 @@ package main;
 
 
 public class DecoderService {
-    
+    String alphabet=new String("аАбБвВгГдДеЕёЁжЖзЗиИйЙкКлЛмМнНоОпПрРсСтТуУфФхХцЦчЧшШщЩъЪыЫьЬэЭюЮяЯ");
+
+    String getDecodedText(String text, int key){
+        String decodedText = new String("");
+        for (int i=0; i<text.length();i++){
+            if (Character.isLetter(text.charAt(i))){
+                decodedText+= alphabet.charAt((alphabet.indexOf(text.charAt(i))+2*key)%66);
+            }
+            else {
+                decodedText+=text.charAt(i);
+            }
+        }
+        return decodedText;
+    }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        DecoderService decoderService = new DecoderService();
 
+        System.out.println(decoderService.getDecodedText("АаБб..ЕеЁё..Яя", 1));
     }
 }
 
